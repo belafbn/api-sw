9f9a37e9fd647cfc29a679829a5121a33cf485c6
Isabela Fabian <isafbn@gmail.com>
2023-10-20 00:10:09 -0300

    so quero me formar :)

--- index.js
+++ index.js
@@ -7,14 +7,15 @@ const swaggerDocs = require('./src/docs/swagger.json')
 require("./connection");
 require("./src/models/Books")
 
-const cors = require('cors')
 
 // cria o servidor
 const app = express()
 
 // configurações do servidor
+const cors = require('cors')
 app.use(express.json())
 app.use(cors())
+
 
 // usa a documentação do swagger (acessivel na rota /docs)
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
